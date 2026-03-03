7aa22c59c35f1cdfe64dbad63f3fd0ff65fd3dfd
Marko Perica <marko.perica2@gmail.com>
2026-03-03 02:25:21 +0100

    suggested deletes

--- src/app/favorites/favorites.ts
+++
@@ -1,8 +0,0 @@
-import { FavoritesService } from './favorites.service'
-import { ProductsService } from '../products.service'
-
-export function favoritesFactory(isFavorite: boolean) {
-    return () => {
-        return isFavorite ? new FavoritesService() : new ProductsService();
-    }
-}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,13 +9,11 @@ import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { SortPipe } from '../sort.pipe';
 import { ProductsService } from '../products.service';
-import { FavoritesComponent } from '../favorites/favorites.component';
-import { ProductViewComponent } from '../product-view/product-view.component';
 
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductDetailComponent, SortPipe, FavoritesComponent, ProductViewComponent],
+  imports: [ProductDetailComponent, SortPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
   providers: [ProductsService]
