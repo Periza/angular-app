daa5612a0d7987a19955d1952864e0421f38ee83
Marko Perica <marko.perica2@gmail.com>
2026-03-31 00:29:39 +0200

    update interceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,5 +1,20 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { catchError, EMPTY, throwError } from 'rxjs';
+
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const authService = inject(AuthService);
+  const authReq = req.clone( {
+    setHeaders: { Authorization: 'myToken' }
+  });
+  return next(authReq).pipe(catchError((error: HttpErrorResponse) => {
+    if(error.status === HttpStatusCode.Unauthorized) {
+      authService.logout();
+      return EMPTY;
+    } else {
+      return throwError(() => error);
+    }
+  }));
 };
